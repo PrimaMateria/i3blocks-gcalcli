205380317b2638f03320aef24af06fb05127a839
matus.benko <matus.benko@gmail.com>
2022-02-13 19:21:31 +0100

    Support clientId and clientSecret

--- i3blocks_gcalcli/cli.py
+++ i3blocks_gcalcli/cli.py
@@ -33,9 +33,9 @@ def add_xterm_parameter(command, param, value):
     command.insert(index, value)
     return command
 
-def get_next_event(event_calendars, status):
+def get_next_event(event_calendars, status, client_id, client_secret):
     process = subprocess.run(add_calendar_parameter(
-            ['gcalcli',  'agenda', '--military', 'now', 'next 3 months', '--tsv'], event_calendars),
+            ['gcalcli',  'agenda', '--military', 'now', 'next 3 months', '--tsv', '--client-id', client_id, '--client-secret', client_secret], event_calendars),
             stdout=subprocess.PIPE,
             universal_newlines=True)
     agenda = process.stdout
@@ -52,13 +52,13 @@ def get_next_event(event_calendars, status):
 
     return status_output
 
-def show_month(month_calendars, font_family, width):
+def show_month(month_calendars, font_family, width, client_id, client_secret):
     right_border = 1
     left_border = 1
     days_on_row = 7 
     xterm_width = (width + left_border) * days_on_row + right_border
 
-    calendar_command = ['gcalcli', 'calm', '--monday', '--military', '--width', str(width)]
+    calendar_command = ['gcalcli', 'calm', '--monday', '--military', '--width', str(width), '--client-id', client_id, '--client-secret', client_secret]
     calendar_command = add_calendar_parameter(calendar_command, month_calendars)
     calendar_output = subprocess.check_output(calendar_command)
 
@@ -74,35 +74,28 @@ def show_month(month_calendars, font_family, width):
 
     subprocess.run(command)
 
-@click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(version="1.0.0")
-def main():
-    pass
-
-@cli.command()
-@click.option("-e", "--eventCalendars", default="", help="Event calendars are considered when getting next upcoming event. Probably weather calendar should be skipped. Multiple values comma separated.")
-@click.option("-m", "--monthCalendars", default="", help="Month calendars are considered when showing full month after click. Multiple values comma separated.")
+@click.command()
+@click.option("-e", "--eventCalendars", default=[], help="Event calendars are considered when getting next upcoming event. Probably weather calendar should be skipped. Possible to provide multiple times.", multiple=True)
+@click.option("-m", "--monthCalendars", default=[], help="Month calendars are considered when showing full month after click. Possible to provide multiple times.", multiple=True)
 @click.option("-f", "--fontFamily", default=None, help="Font family/face used for xterm window showing month calendar.")
 @click.option("-w", "--width", default=20, type=int, help="Cell width of month calendar. Minimum 10.")
-@click.option("-s", "--status", default="datetimetitle",  type=click.Choice(['datetimetitle', 'datetitle', 'titlte'], help="Status format.")
-def print(**kwargs):
-    event_calendars = kwargs["eventCalendars"].split(",")
-    month_calendars = kwargs["monthCalendars"].split(",")
-    font_family = kwargs["fontFamily"]
+@click.option("-s", "--status", default="datetimetitle", type=click.Choice(['datetimetitle', 'datetitle', 'titlte']), help="Status format.")
+@click.option("--clientId", help="Google API Client ID.")
+@click.option("--clientSecret", help="Google API Client Secret.")
+def main(**kwargs):
+    event_calendars = kwargs["eventcalendars"]
+    month_calendars = kwargs["monthcalendars"]
+    font_family = kwargs["fontfamily"]
     width = kwargs["width"]
     status = kwargs["status"]
+    client_id = kwargs["clientid"]
+    client_secret = kwargs["clientsecret"]
 
-    print(event_calendars)
-    print(month_calendars)
-    print(font_family)
-    print(width)
-    print(status)
+    block_button = os.environ.get('BLOCK_BUTTON')
+    if (block_button in ["1", "2", "3"]):
+        show_month(month_calendars, font_family, width, client_id, client_secret)
 
-    # block_button = os.environ.get('BLOCK_BUTTON')
-    # if (block_button in ["1", "2", "3"]):
-        # show_month(month_calendars, font_family, width)
-
-    # print(get_next_event(event_calendars, status))
+    print(get_next_event(event_calendars, status, client_id, client_secret))
 
 
 if __name__ == "__main__":
